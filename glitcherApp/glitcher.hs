bd7a1ba23e3b12dbc1d8fedac50de9a54324104f
James Anaipakos <janaipakos@gmail.com>
2017-03-23 18:49:31 -0400

    new options

--- glitcherApp/glitcher.hs
+++ glitcherApp/glitcher.hs
@@ -10,38 +10,65 @@ intToC :: Int -> C.ByteString
 intToC int = C.pack [intToChar int]
 
 --split given bytestring, replace one of them, concat
-byteReplace :: Int -> Int -> C.ByteString -> C.ByteString 
-byteReplace location charVal bytes = mconcat [before,newChar,after]
+replace :: Int -> Int -> C.ByteString -> C.ByteString 
+replace location charVal bytes = mconcat [before,newChar,after]
     where (before,rest) = C.splitAt location bytes
           after = C.drop 1 rest
           newChar = intToC charVal
 
 --pick a byte between 1..byteLength and replace it with 0..255
-byteReplaceRandom :: C.ByteString -> IO C.ByteString 
-byteReplaceRandom bytes = do
+randomReplace :: C.ByteString -> IO C.ByteString 
+randomReplace bytes = do
     let bytesLength = C.length bytes
     location <- randomRIO (1,bytesLength) 
     charVal <- randomRIO (0,255)
-    return (byteReplace location charVal bytes)
+    return (replace location charVal bytes)
 
 --similar to replaceByte
-sectionSort :: Int -> Int -> C.ByteString -> C.ByteString 
-sectionSort start size bytes = mconcat [before,changed,after]
+transpose :: Int -> Int -> C.ByteString -> C.ByteString 
+transpose start size bytes = mconcat [before,changed,after]
     where (before,rest) = C.splitAt start bytes 
           (target,after) = C.splitAt size rest 
           changed = C.reverse $ C.sort target
 
-sectionSortRandom :: C.ByteString -> IO C.ByteString 
-sectionSortRandom bytes = do
+randomTranspose :: C.ByteString -> IO C.ByteString 
+randomTranspose bytes = do
     let sectionSize = 25
     let bytesLength = C.length bytes
     start <- randomRIO (0, bytesLength - sectionSize)
-    return (sectionSort start sectionSize bytes)
+    return (transpose start sectionSize bytes)
 
+--delete bytestring
+defect :: Int -> Int -> C.ByteString -> C.ByteString 
+defect start size bytes = mconcat [before,emptyByte,after]
+    where (before,rest) = C.splitAt start bytes 
+          (target,after) = C.splitAt size rest
+          emptyByte = C.drop (C.length target + 1) target
+
+--randomly pick location to delete
+randomDefect :: C.ByteString -> IO C.ByteString 
+randomDefect bytes = do
+    let sectionSize = 25
+    let bytesLength = C.length bytes
+    start <- randomRIO (0, bytesLength - sectionSize)
+    return (defect start sectionSize bytes)
+
+--specify which and number of glitches 
 imageGlitch :: [C.ByteString -> IO C.ByteString]
-imageGlitch = [byteReplaceRandom
-              ,sectionSortRandom
+imageGlitch = [randomReplace
+              ,randomTranspose
+              ,randomDefect
               ]
+
+--specify which and number of glitches 
+imageGlitch2 :: [C.ByteString -> IO C.ByteString]
+imageGlitch2 = [randomReplace
+                ,randomReplace
+                ,randomReplace
+                ,randomReplace
+                ,randomReplace
+                ,randomReplace
+                ]
 
 glitcher :: FilePath -> IO ()
 glitcher imageFileName = do
@@ -50,8 +77,19 @@ glitcher imageFileName = do
     let glitchedFileName = mconcat ["glitched_",imageFileName]
     C.writeFile glitchedFileName glitched
 
+glitcher2 :: FilePath -> IO ()
+glitcher2 imageFileName = do
+    imageFile <- C.readFile imageFileName
+    glitched <- foldM (\bytes func -> func bytes) imageFile imageGlitch2
+    let glitchedFileName = mconcat ["glitched_",imageFileName]
+    C.writeFile glitchedFileName glitched
+
 main :: IO ()
 main = do
+    print "pick a glitcher type. 1 is simple, anything else is replace*6"
     imageFiles <- getArgs
-    mapM_ glitcher imageFiles
+    glitcherType <- getLine
+    if glitcherType == "1" 
+    then mapM_ glitcher imageFiles
+    else mapM_ glitcher2 imageFiles
     print "completed image glitching"
